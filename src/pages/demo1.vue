8bbbd97ce65e3abdacad9ed6cd6ecc6b3094a267
SimonLiang <liangshaoming20@163.com>
2020-04-23 19:22:17 +0800

    feat: 完善拖拽功能

--- src/pages/demo1.vue
+++ src/pages/demo1.vue
@@ -1,15 +1,8 @@
 <template>
   <div class="container">
-    <el-table
-      class="table-box drag-table"
-      :data="classTable"
-      border
-      :show-header="false"
-      :cell-class-name="getCellClassName"
-    >
-      <el-table-column prop="date" label="日期" width="180"></el-table-column>
-      <el-table-column prop="name" label="姓名" width="180"></el-table-column>
-      <el-table-column prop="address" label="地址" width="180"></el-table-column>
+    <h1>计划表-支持拖拽</h1>
+    <el-table class="drag-table" :data="schedule" border :cell-class-name="getCellClassName">
+      <el-table-column v-for="item in x" :key="item.value" :prop="item.value" :label="item.label"></el-table-column>
     </el-table>
   </div>
 </template>
@@ -21,31 +14,47 @@ Sortable.mount(new MultiDrag(), new Swap());
 export default {
   data() {
     return {
-      classTable: [
+      x: [
+        { value: "time", label: "" },
+        { value: "mon", label: "星期一" },
+        { value: "tue", label: "星期二" },
+        { value: "wed", label: "星期三" },
+        { value: "thu", label: "星期四" },
+        { value: "fri", label: "星期五" },
+        { value: "sat", label: "星期六" },
+        { value: "sun", label: "星期日" }
+      ],
+      schedule: [
         {
-            weekDay: "mon",
-            
+          time: "9:00~10:00",
+          mon: "语文",
+          tue: "",
+          wed: "",
+          thu: "",
+          fri: "",
+          sat: "",
+          sun: ""
+        },
+        {
+          time: "10:00~11:00",
+          mon: "",
+          tue: "英语",
+          wed: "",
+          thu: "",
+          fri: "",
+          sat: "",
+          sun: ""
+        },
+        {
+          time: "11:00~12:00",
+          mon: "",
+          tue: "",
+          wed: "数学",
+          thu: "",
+          fri: "化学",
+          sat: "",
+          sun: ""
         }
-        // {
-        //   date: "",
-        //   name: "小明",
-        //   address: ""
-        // },
-        // {
-        //   date: "",
-        //   name: "小虎",
-        //   address: ""
-        // },
-        // {
-        //   date: "",
-        //   name: "小黄",
-        //   address: ""
-        // },
-        // {
-        //   date: "",
-        //   name: "小刘",
-        //   address: ""
-        // }
       ]
     };
   },
@@ -71,12 +80,12 @@ export default {
         Sortable.create(el, {
           group: {
             name: "shared",
-            pull: "clone"
+            // pull: "clone"
           },
           swap: true,
           filter: ".filtered",
           onEnd: evt => {
-            console.log("onEnd", evt);
+            // console.log("onEnd", evt);
             this.updateData({
               oldRowIndex: evt.from.rowIndex,
               newRowIndex: evt.to.rowIndex,
@@ -88,7 +97,25 @@ export default {
       }
     },
     updateData({ oldRowIndex, newRowIndex, oldColumnIndex, newColumnIndex }) {
-      console.log(oldRowIndex, newRowIndex, oldColumnIndex, newColumnIndex);
+      // console.log(oldRowIndex, newRowIndex, oldColumnIndex, newColumnIndex);
+
+      // 交换数据
+      const schedule = JSON.parse(JSON.stringify(this.schedule));
+      const OldProperty = this.x[oldColumnIndex].value;
+      const oldValue = schedule[oldRowIndex][OldProperty];
+      const newProperty = this.x[newColumnIndex].value;
+      const newValue = schedule[newRowIndex][newProperty];
+      schedule[newRowIndex][newProperty] = oldValue;
+      schedule[oldRowIndex][OldProperty] = newValue;
+
+      // 强刷
+      this.schedule = [];
+      this.$nextTick(() => {
+        this.schedule = schedule;
+        this.$nextTick(() => {
+          this.initDrag();
+        });
+      });
     }
   }
 };
@@ -96,12 +123,9 @@ export default {
 
 <style lang="scss" scoped>
 .container {
-  padding-left: 30px;
+  padding: 0 30px;
   background: #f1eded;
   height: 500px;
   padding-top: 30px;
-  .table-box {
-    width: 550px;
-  }
 }
 </style>
